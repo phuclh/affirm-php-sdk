4c51906f06b7dfa0c3c0ac982ab5fa7a9454e2d9
Mike Funk <mike@mikefunk.com>
2017-11-30 17:32:39 -0800

    init

---
+++ src/Affirm/Api/Client.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Client
+ *
+ * @copyright Leaf Group, Ltd. All Rights Reserved.
+ */
+declare(strict_types=1);
+
+namespace Affirm\Api;
+
+/**
+ * Interact with the affirm api.
+ *
+ * @author Michael Funk <mike.funk@leafgroup.com>
+ */
+class Client
+{
+
+}
